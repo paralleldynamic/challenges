98841eef6afae9dd81119f66ec536e3d22b7cd6c
Patrick Spears <spearspa@gmail.com>
2019-12-06 22:48:59 -0600

    efficient passing part 1

--- codevent/2019/04/PasswordCracker.py
+++ codevent/2019/04/PasswordCracker.py
@@ -1,26 +1,15 @@
 input = list(map(str,range(347312,805915)))
 
-def two_digits(n):
-    i = 0
-    for i in range(len(n)):
-        if i == 0:
-            continue
-        if n[i] == n[i - 1]:
-            return True
-    return False
-
-def incrementing(n):
-    i = 0
-    for i in range(len(n)):
-        if i == 0:
-            continue
-        if n[i] < n[i-1]:
+def test_rules(n):
+    consecutive = False
+    for x, y in zip(n, n[1:]):
+        if x > y:
             return False
-    return True
+        if x == y:
+            consecutive = True
+    return consecutive
 
 if __name__ == '__main__':
-    for n in input:
-        if not (two_digits(n) and incrementing(n)):
-            input.remove(n)
+    passing = list(filter(test_rules, input))
     
-    print(input)
+    print(len(passing))
