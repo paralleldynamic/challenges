4f9a3d041bfedbffbc945a0a85c8eb0d784774b2
Patrick Spears <paralleldynamic@users.noreply.github.com>
2021-12-14 12:49:07 -0500

    build: solve for part 1

---
+++ codevent/2021/14/polymerization.py
@@ -0,0 +1,72 @@
+from collections import Counter
+from functools import reduce
+from itertools import chain
+
+
+def generate_pairs(polymer_tempalte):
+    pairs = list(zip(polymer_tempalte, polymer_tempalte[1:]))
+    return pairs
+
+
+def polymerize(pair, pair_insertion_rules):
+    p = "".join(pair)
+    insertion_element = pair_insertion_rules[p]
+    insertion = (pair[0], insertion_element, pair[-1])
+    return insertion
+
+
+def synthesize_pairs(pairs):
+    joiner = lambda x: "".join(x)
+    last_element = pairs[-1][-1]
+    elements = map(lambda x: x[:-1], pairs)
+    polymer = joiner(map(joiner, elements)) + last_element
+    return polymer
+
+
+def generate_polymer(initial_pairs, rules, iterations):
+    polymer_pairs = initial_pairs
+    for i in range(iterations):
+        trips = []
+        for pair in polymer_pairs:
+            trips.append(polymerize(pair, rules))
+        polymer_string = synthesize_pairs(trips)
+        polymer_pairs = generate_pairs(polymer_string)
+    return polymer_pairs
+
+
+class InputReader():
+    def __init__(self, input_file_path="input.txt"):
+        self.pair_insertion_rules = {}
+        self.ingest_data(input_file_path)
+
+    def ingest_data(self, input_file_path):
+        with open(input_file_path, "r") as input:
+            self.polymer_template = input.readline().strip()
+            # _ = input.readline() # blank line to separate template from rules
+            for rule in input:
+                if rule.strip():
+                    pair, insertion = rule.strip().split(" -> ")
+                    self.pair_insertion_rules[pair] = insertion
+            self.initial_pairs = self.generate_pairs(self.polymer_template)
+
+    def generate_pairs(self, polymer_tempalte):
+        pairs = list(zip(polymer_tempalte, polymer_tempalte[1:]))
+        return pairs
+
+    def polymerize(self, pair):
+        p = self._join(pair)
+        insertion_element = self.pair_insertion_rules[p]
+        insertion = (pair[0], insertion_element, pair[-1])
+        return insertion
+
+    def _join(self, elements):
+        return "".join(elements)
+
+if __name__ == "__main__":
+    ir = InputReader()
+    ip = ir.initial_pairs
+    rules = ir.pair_insertion_rules
+    polymer_pairs = generate_polymer(ip, rules, 40)
+    polymer = synthesize_pairs(polymer_pairs)
+    c = Counter(polymer)
+    print(max(c.values()) - min(c.values()))
